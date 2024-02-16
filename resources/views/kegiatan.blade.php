9402cb34e63c643c232f5144b5b2847af684e801
unknown <afiffakhriyadh2012@gmail.com>
2024-02-16 07:57:09 +0700

    done

--- resources/views/kegiatan.blade.php
+++ resources/views/kegiatan.blade.php
@@ -34,71 +34,35 @@
                 </div>
                 <div class="col-md-3 col-sm-12 my-5 px-2">
                     <div class="card rounded shadow "  style="border: 1px solid red;">
-                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/10/WhatsApp-Image-2023-10-25-at-10.22.57-wien.jpeg"
+                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/01/all-750x430.jpg"
                             alt="image" class="rounded">
                             <div class="card-body">
-                                <h4 class="card-title">Kegiatan</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ratione aut deserunt, commodi ullam nulla!</p>
+                                <h4 class="card-title"> PT Telkom CDC Berikan Tab Untuk Aplikasi IChat Kepada Lembaga Disabilitas</h4>
+                                <p class="card-text">SMK Telkom Jakarta menyalurkan bantuan Tablet Samsung Galaxy Tab A8 kepada perwakilan 8 (delapan) Lembaga disabilitas di Provinsi DKI Jakarta di Ruang Rapat SMK Telkom Jakarta. Bantuan Tablet ini bersumber dari PT Telkom CDC.
+                                    PT. Telkom Indonesia adalah BUMN yang memiliki komitmen untuk dapat selalu dekat dengan lingkungan sekitar maupun masyarakat luas melalui kegiatan TJSL (Tanggung Jawab Sosial Lingkungan) atau biasa disebut dengan CSR (Corporate Social Responsibility). Unit yang menjalankan program TJSL yakni CDC (Community Development Center).
+                                   sehingga UMK yang ada dapat berkembang dan menghasilkan sebuah produk yang berkualitas dan bisa bersaing di pasar.</p>
                             </div>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-12 my-5 px-2">
                     <div class="card rounded shadow "  style="border: 1px solid red;">
-                        <img src="https://scontent-sin6-1.xx.fbcdn.net/v/t39.30808-6/323136986_1350746869029405_5568785056163240281_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=783fdb&_nc_ohc=HVNJzi2SCXYAX_-EEMr&_nc_ht=scontent-sin6-1.xx&oh=00_AfBZ4ppnkLkDbuqfnI_N3F7JpvFOm_FXsnyNfAA6sY2XoA&oe=65D04952"
+                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/09/WhatsApp-Image-2023-09-13-at-3.14.36-PM1-750x430.jpeg"
                             alt="image" class="rounded">
                             <div class="card-body">
-                                <h4 class="card-title">Kegiatan</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ratione aut deserunt, commodi ullam nulla!</p>
+                                <h4 class="card-title">ICS COMPUTE And AWS AUTHORIZED INSTRUCTOR Hadir Di SMK Telkom Jakarta</h4>
+                                <p class="card-text">SMK Telkom Jakarta, sesuai program kegiatan belajar mengajar Sentra kelas XI, pada tanggal 13/09/2023 PT ICS Compute dan AWS Authorized Instructor menyelenggarakan kegiatan Stadium General di GOR SMK Telkom Jakarta yang dimulai dari jam 09.00-11.00 WIb.
+                                    Pada bulan November 2020, PT. Innovation Cloud Services (ICS Compute) telah menerima Amazon Web Services (AWS) Partner Network (APN) Rising Star of The Year Award 2020 untuk regional ASEAN pada AWS Partner Awards tahunan.
+                                    Selama dua tahun berturut-turut, perusahaan tersebut dianugerahi Amazon Web Services (AWS)</p>
                             </div>
                     </div>
                 </div>
                 <div class="col-md-3 col-sm-12 my-5 px-2">
                     <div class="card rounded shadow "  style="border: 1px solid red;">
-                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/01/IMG_8235-scaled.jpg"
+                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/01/DSC_5650-scaled.jpg"
                             alt="image" class="rounded">
                             <div class="card-body">
-                                <h4 class="card-title">Kegiatan</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ratione aut deserunt, commodi ullam nulla!</p>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-12 my-5 px-2">
-                    <div class="card rounded shadow "  style="border: 1px solid red;">
-                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/01/IMG_8235-scaled.jpg"
-                            alt="image" class="rounded">
-                            <div class="card-body">
-                                <h4 class="card-title">Kegiatan</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ratione aut deserunt, commodi ullam nulla!</p>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-12 my-5 px-2">
-                    <div class="card rounded shadow "  style="border: 1px solid red;">
-                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/01/IMG_8235-scaled.jpg"
-                            alt="image" class="rounded">
-                            <div class="card-body">
-                                <h4 class="card-title">Kegiatan</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ratione aut deserunt, commodi ullam nulla!</p>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-12 my-5 px-2">
-                    <div class="card rounded shadow "  style="border: 1px solid red;">
-                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/01/IMG_8235-scaled.jpg"
-                            alt="image" class="rounded">
-                            <div class="card-body">
-                                <h4 class="card-title">Kegiatan</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ratione aut deserunt, commodi ullam nulla!</p>
-                            </div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-sm-12 my-5 px-2">
-                    <div class="card rounded shadow "  style="border: 1px solid red;">
-                        <img src="https://smktelkom-jkt.sch.id/wp-content/uploads/2023/01/IMG_8235-scaled.jpg"
-                            alt="image" class="rounded">
-                            <div class="card-body">
-                                <h4 class="card-title">Kegiatan</h4>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ratione aut deserunt, commodi ullam nulla!</p>
+                                <h4 class="card-title">Pembinaan Karakter Oleh TNI Kepada Peserta Didik SMK Telkom Jakarta</h4>
+                                <p class="card-text">SMK Telkom Jakarta Minggu (08/01/2023) melakukan kegiatan pembinaan karakter yang dibimbing langsung oleh Kodim 0503/JB. Danramil 04 Cengkareng Kapten Inf Kurniawan bersama Babinsa memberikan latihan dasar kepemimpinan (LDKS) kepada siswa-siswi SMK Telkom Jakarta, berjumlah 420 orang, pelaksanaan yang dimulai sejak Sabtu kemarin dan berakhir Minggu bertempat di lingkungan SMK Telkom Jakarta Kelurahan Kedaung Kaliangke Kecamatan Cengkareng, Jakarta Barat. </p>
                             </div>
                     </div>
                 </div>
